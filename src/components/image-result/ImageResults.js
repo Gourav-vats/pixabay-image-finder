6a343de2d9beb9a3d2e1745bdf6221f8c4cdf404
Gourav Vats <gourav4142@gmail.com>
2021-08-16 20:05:15 +0530

    Search results successfully rendered

--- src/components/image-result/ImageResults.js
+++ src/components/image-result/ImageResults.js
@@ -4,35 +4,35 @@ import {
    ImageListItem,
    ImageListItemBar,
    IconButton,
+   Grid,
 } from "@material-ui/core";
-import { ZoomIn } from "@material-ui/icons";
+import ZoomInTwoToneIcon from "@material-ui/icons/ZoomInTwoTone";
 
 const ImageResults = (props) => {
    const { imagesData } = props;
    return (
-      <ImageList cols={3} rowHeight={100} >
-         {imagesData.map((image) => {
-            <ImageListItem cols={1} key={image.id} rowHeight={100}>
-               {console.log(image.largeImageURL)}
-               {console.log(image.id)}
-               <img src={image.largeImageURL} alt="" />
-            </ImageListItem>;
-
-            <ImageListItemBar
-               title={image.tags}
-               subtitle={
-                  <span>
-                     by <strong>{image.user}</strong>
-                  </span>
-               }
-               actionIcon={
-                  <IconButton>
-                     <ZoomIn />
-                  </IconButton>
-               }
-            />;
-         })}
-      </ImageList>
+      <Grid md={2}>
+         <ImageList cols={12}>
+            {imagesData.map((image) => (
+               <ImageListItem key={image.id} cols={3}>
+                  <img src={image.largeImageURL} alt={image.type} />
+                  <ImageListItemBar
+                     title={image.tags}
+                     subtitle={
+                        <span>
+                           by <strong>{image.user}</strong>
+                        </span>
+                     }
+                     actionIcon={
+                        <IconButton>
+                           <ZoomInTwoToneIcon color="white" />
+                        </IconButton>
+                     }
+                  />
+               </ImageListItem>
+            ))}
+         </ImageList>
+      </Grid>
    );
 };
 
