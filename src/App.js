1ee214cf8dbee92e886192f396467ef8d11b611c
Gourav Vats <gourav4142@gmail.com>
2021-08-17 20:23:36 +0530

    unnecessary files deleted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Search from "./components/search/Search";
 import { MuiThemeProvider, Container } from "@material-ui/core";
